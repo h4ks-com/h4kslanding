004ed23c3cd119c8e6995909314b678cbd91dde7
Christopher Handy <handyc@helxt.com>
2024-07-24 21:34:56 +0000

    adding models

--- interface/webapp/admin.py
+++ interface/webapp/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+
+admin.site.site_header = "h4ks Landing Page";
+admin.site.site_title = "h4ks Landing Page";
+
+from .models import Location
+from .models import App
+
+@admin.register(Location)
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ('name','zone','weight','color',)
+    list_filter = ('name','zone','weight','color',)
+    fields = ('name', 'zone','weight','color',)
+
+@admin.register(App)
+class AppAdmin(admin.ModelAdmin):
+    list_display = ('name','location','weight','color',)
+    list_filter = ('name','location','weight','color',)
+    fields = ('name', 'location','weight','color',)
+
--- interface/webapp/models.py
+++ interface/webapp/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+
+class Location(models.Model):
+    class Meta:
+        ordering = ('weight',)
+        verbose_name_plural = "Locations"
+
+    name = models.CharField(max_length=300, null=True, blank=True)
+    zone = models.CharField(max_length=300, null=True, blank=True)
+    weight = models.IntegerField(default=0, null=True, blank=True)
+    color = models.CharField(max_length=300, null=True, blank=True)
+
+    #def f(x):
+    #    x
+
+    def __str__(self):
+        return self.name
+
+class App(models.Model):
+    class Meta:
+        ordering = ('weight',)
+        verbose_name_plural = "Apps"
+
+    name = models.CharField(max_length=300, null=True, blank=True)
+    location = models.CharField(max_length=300, null=True, blank=True)
+    #zone = models.CharField(max_length=300, null=True, blank=True)
+    weight = models.IntegerField(default=0, null=True, blank=True)
+    color = models.CharField(max_length=300, null=True, blank=True)
+
+    #def f(x):
+    #    x
+
+    def __str__(self):
+        return self.name
+
+
