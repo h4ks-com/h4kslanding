207acfae99a5c37ae9a9aadada9374725e99de47
doesnm <doesnm@0ut0f.space>
2025-06-30 09:52:03 +0300

    make env var for debug

--- interface/h4kslanding/settings.py
+++ interface/h4kslanding/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('SECRET_KEY')
 
 #ALLOWED_HOSTS = ['h4ks.com',]
 ALLOWED_HOSTS = ['*',]
