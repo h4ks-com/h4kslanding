8b4fcfd20e6279131d9f988d6b48a6a1a46a8b61
Matheus Fillipe <matheusfillipeag@gmail.com>
2026-04-06 23:24:51 +0200

    fix path

--- interface/h4kslanding/urls.py
+++ interface/h4kslanding/urls.py
@@ -4,11 +4,12 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.0/topics/http/urls/
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('', include('webapp.urls')),
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
